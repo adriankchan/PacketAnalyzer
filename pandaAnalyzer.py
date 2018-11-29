0a1463b25b9f47b82362b1d333bdd660c8e974f7
Adrian <adrian.chan@ucalgary.ca>
2018-11-28 21:42:45 -0700

    Made packet dataframe more searchable

--- pandaAnalyzer.py
+++ pandaAnalyzer.py
@@ -14,21 +14,51 @@ def filter_by_protocol(df, protocol):
     return filtered
 
 
-# Search for the packets using the following search terms
-# SYN
-# SYN, ACK
-# FIN, ACK
-def filter_by_tcp_exchange(df, search_term):
-    filtered = df[df['Info'].str.contains(search_term)]
-    return filtered
+# Function to extract the important values from the info column
+def extract_values(row):
+
+    delimited = row["Info"].split('[')[1].replace(']','')
+    delimited = delimited.replace(', ',',')
+    data = delimited.split(' ')
+
+    if len(data) == 5:
+        flag = data[0]
+        seq = int(data[1].split('=')[1])
+        ack = None
+        win = int(data[2].split('=')[1])
+        length = int(data[3].split('=')[1])
+    else:
+        flag = data[0]
+        seq = int(data[1].split('=')[1])
+        ack = int(data[2].split('=')[1])
+        win = int(data[3].split('=')[1])
+        length = int(data[4].split('=')[1])
+
+    output = pd.DataFrame([flag, seq, ack, win, length]).T
+    output.columns = ['Flag', 'SEQ', 'ACK', 'WIN', 'LEN']
+
+    return output
+
+# Convert the Info column into searchable columns
+def make_dataframe_searchable(df):
+    temp = pd.DataFrame()
+    for index, row in df.iterrows():
+        temp = temp.append(extract_values(row))
+
+    df.reset_index(drop=True, inplace=True)
+    temp.reset_index(drop=True, inplace=True)
+    df2 = df.join(temp)
+
+    return df2
 
 
 # Import CSV Files
 file_name = 'ServerTraffic-F2018.csv'
 df = pd.read_csv(file_name)
 
-# Run the desired IP through the filtering
 ip_filtered = filter_by_ip(df,'150.100.12.11', '150.100.0.2')
 protocol_filtered = filter_by_protocol(ip_filtered, 'TCP')
 
-output = filter_by_tcp_exchange(protocol_filtered, 'FIN, ACK')
+# Run the desired IP through the filtering
+df2 = make_dataframe_searchable(protocol_filtered)
+print(df2)
